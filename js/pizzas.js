a160eaf806cc2f2d34e8af0f3aebf45624753027
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-09-08 20:28:28 -0300

    Update pizzas.js

--- js/pizzas.js
+++ js/pizzas.js
@@ -1,6 +1,31 @@
 let pizzaJson = [
   {
-    id:1, name:'Mussarela', img:imagens/pizza
-    ,
-  }
+    id:1, name:'Mussarela', img:'imagens/pizza.png', price:25.00, sizes:['100g',
+    '530g','860g'], description:'Molho de tomate, camada dupla de mussarela e orégano'
+  },
+  {
+    id:2, name:'Calabresa',img:'imagens/pizza2.png',price:26.00, sizes:['320g',
+    '530g','860g'], description:''
+  },
+  {
+    id:3, name:'Quatro Queijos',img:'imagens/pizza3.png',price:28.00 , sizes:['320g',
+    '530g','860g'], description:''
+  },
+  {
+    id:4, name:'Brasileira',img:'imagens/pizza4.png',price:30.00, sizes:['320g',
+    '530g','860g'], description:''
+  },
+  {
+    id:5, name:'Portuguesa',img:'imagens/pizza5.png',price:29.00, sizes:['320g',
+    '530g','860g'], description:''
+  },
+  {
+    id:6, name:'Moda da Casa',img:'imagens/pizza6.png',price:35.00, sizes:['320g',
+    '530g','860g'], description:''
+  },
+  {
+    id:7, name:'Banana com Canela',img:'imagens/pizza7.png',price:  , sizes:['320g',
+    '530g','860g'], description:''
+  },
+  
 ]
