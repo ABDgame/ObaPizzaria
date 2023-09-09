d1db11436730a2f5389e8b3b3bf112bdbd76545e
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-09-09 20:34:59 -0300

    Update pizzas.js

--- js/pizzas.js
+++ js/pizzas.js
@@ -5,27 +5,32 @@ let pizzaJson = [
   },
   {
     id:2, name:'Calabresa',img:'imagens/pizza2.png',price:26.00, sizes:['320g',
-    '530g','860g'], description:'Molho de tomate, '
+    '530g','860g'], description:'Molho de tomate, mussarela, calabresa fatiada, 
+    cebola e orégano'
   },
   {
     id:3, name:'Quatro Queijos',img:'imagens/pizza3.png',price:28.00 , sizes:['320g',
-    '530g','860g'], description:'Molho de tomate,'
+    '530g','860g'], description:'Molho de tomate, camadas de mussarela, provolone,
+     parmesão, gorgonzola e orégano'
   },
   {
     id:4, name:'Brasileira',img:'imagens/pizza4.png',price:30.00, sizes:['320g',
-    '530g','860g'], description:'Molho de tomate,'
+    '530g','860g'], description:'Molho de tomate, mussarela, calabresa picada,
+      palmito, champignon, azeitonas e orégano'
   },
   {
     id:5, name:'Portuguesa',img:'imagens/pizza5.png',price:29.00, sizes:['320g',
-    '530g','860g'], description:'Molho de tomate,'
+    '530g','860g'], description:'Molho de tomate, mussarela, presunto, ovos,
+      cebolas, pimentão, azeitona e orégano'
   },
   {
     id:6, name:'Moda da Casa',img:'imagens/pizza6.png',price:35.00, sizes:['320g',
-    '530g','860g'], description:'Mussarela, banana, canela'
+    '530g','860g'], description:'Molho de tomate, mussarela, carne de sol,
+    tomates em cubos, coentro, cebola, azeitona, catupiry e oregano'
   },
   {
     id:7, name:'Banana com Canela',img:'imagens/pizza7.png',price:  , sizes:['320g',
-    '530g','860g'], description:''
+    '530g','860g'], description:'Mussarela, banana, canela, '
   },
   
 ]
