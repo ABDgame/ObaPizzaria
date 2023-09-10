8e2e2a5e692b518f4777ec588e61087c26b4ca1b
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-09-09 22:00:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+pizzaJson.map((item,index ) => {
+  //console.log(item)
 
+  let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+  //console.log(pizzaItem)
+
+  document.querySelector('.pizza-area').append(pizzaItem)
+
+  //preencher os dados de cada pizza
+  pizzaItem.querySelector('.pizza-item--img ')
+
+
+
+
+})
