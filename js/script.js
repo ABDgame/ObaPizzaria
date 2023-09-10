e8c51652426ffcd773307affe9626cac6e6ec675
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-09-09 22:09:25 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,9 +7,10 @@ pizzaJson.map((item,index ) => {
   document.querySelector('.pizza-area').append(pizzaItem)
 
   //preencher os dados de cada pizza
-  pizzaItem.querySelector('.pizza-item--img ')
-
-
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img
+  pizzaItem.querySelector('.pizza-item--price').innerHtml = item.price
+  pizzaItem.querySelector('.pizza-item--name').innerHtml = item.name
+  pizzaItem.querySelector('.pizza-item--desc').innerHtml = item.description
 
 
 })
