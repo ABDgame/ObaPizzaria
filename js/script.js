d83fe4e5dba1af4fefb8cfdacf10608c259333f1
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-09-10 19:05:14 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,5 +13,19 @@ pizzaJson.map((item,index ) => {
   pizzaItem.querySelector('.pizza-item--name').innerHtml = item.name
   pizzaItem.querySelector('.pizza-item--desc').innerHtml = item.description
 
-
+  //pizza clicada
+  pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
+      e.preventDefault()
+      console.log('Clicou na pizza')
+     
+      //abrir janela modal
+      document.querySelector('.pizzaWindowArea').style.display = 'flex'
+      //preenchimento de dados
+  })
+ document.querySelector('.pizzaInfo--cancelButton').addEventListener('click', () => {
+   document.querySelector('.pizzaWindowArea').style.display = 'none'
+ })
+ document.querySelector('.pizzaInfo--cancelMobileButton').addEventListener('click', () => {
+   document.querySelector('.pizzaWindowArea').style.display = 'none'
+ })
 })
