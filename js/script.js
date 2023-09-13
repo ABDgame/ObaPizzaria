e5f07b578b5d0bf04593ba29a4935d55e88ab41d
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-09-12 21:18:12 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,13 +28,13 @@ pizzaJson.map((item,index ) => {
 
   let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
   //console.log(pizzaItem)
+  //document.querySelector('.pizza-area').append(pizzaItem)
+  seleciona('.pizza-area').append(pizzaItem)
 
-  document.querySelector('.pizza-area').append(pizzaItem)
 
   //preencher os dados de cada pizza
   pizzaItem.querySelector('.pizza-item--img img').src = item.img
-  pizzaItem.querySelector('.pizza-item--price').innerHtml = 'R$ ${item.price.
-  toFixed(2)}'
+  pizzaItem.querySelector('.pizza-item--price').innerHtml = 'R$ ${item.price.toFixed(2)}'
   pizzaItem.querySelector('.pizza-item--name').innerHtml = item.name
   pizzaItem.querySelector('.pizza-item--desc').innerHtml = item.description
 
