66330500713b91d5dec7bf3ac2c7de175369246a
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-11-12 21:23:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,4 +8,17 @@ pagination{
   el: ".swiper-pagination",
   clickable true,
  }
-}),
+});
+
+/* ========= Scroll header ========= */
+let header = document.querySelector('header');
+
+function scrollHeader(){
+  let scrollY = window.pageYOffset;
+
+  if(scrollY > 30) {
+    header.classList.add('active');
+  }else{
+   header.classList.remove('active');
+  }
+}
